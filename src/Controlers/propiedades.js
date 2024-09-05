c4205898d5897d87ab91fc739ab27a94db359470
fmarcos38 <fmarcos_23@hotmail.com>
2024-09-05 19:58:16 -0300

    limit

--- src/Controlers/propiedades.js
+++ src/Controlers/propiedades.js
@@ -19,33 +19,35 @@ const url = process.env.URL;
 //trae propiedades
 const getProperties = async(req, res) => { 
     const {limit, offset, operacion, tipo, precioMin, precioMax} = req.query; 
-    
+//ver error de limit
     try {
         let resp;
-        let newArray;
+        let total;
+        let propiedades;
 
-        if(limit !== 0 && offset !== 0){
+        if(Number(limit) !== 0 || Number(offset) !== 0){
             resp = await axios.get(`${url}&limit=${limit}&offset=${offset}&key=${apiKey}`);
         }else{
             resp = await axios.get(`${url}&key=${apiKey}`);
         }        
         //normalizo data q me llega
-        newArray = normalizaProps(resp.data.objects);
+        total = resp.data.meta.total_count;        
+        propiedades = normalizaProps(resp.data.objects); 
 
         // Filtros
         //por operación
         if(operacion) { 
-            newArray = newArray.filter(p => 
+            propiedades = propiedades.filter(p => 
                 p.operacion.some(item => item.operacion === operacion)
             );
         }
         //por tipo de propiedad
         if(tipo) {
-            newArray = newArray.filter(p => p.tipo.nombre === tipo);
+            propiedades = propiedades.filter(p => p.tipo.nombre === tipo);
         }
         //por precios min y max
         if(precioMin && precioMax) {
-            newArray = newArray.filter(p => 
+            propiedades = propiedades.filter(p => 
                 p.operacion.some(item => 
                     item.precios.some(precio => 
                         precio.precio >= Number(precioMin) && precio.precio <= Number(precioMax)
@@ -53,7 +55,11 @@ const getProperties = async(req, res) => {
                 )
             );
         }
-        res.json(newArray);
+
+        res.json({
+            total,
+            propiedades
+        });
     } catch (error) {
         console.log(error);
     }
